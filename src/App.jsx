fce3b2fcea8b51649084ec5b7a5ddae9464ff43d
rifan <tararifan93@gmail.com>
2025-09-03 14:42:28 +0700

    feat: add initial UI layout for Weather App
    - setup header with app title
    - add search box with input & button
    - prepare containers for loading, error, and weather data
    - include basic state hooks for city, weatherData, loading, and error

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,38 @@
 // import { useState } from 'react'
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-    return <></>;
+    const [weatherData, setWeatherData] = useState(null);
+    const [city, setCity] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    return (
+        <div className="app-container">
+            <header>
+                <h1>Modern Weather App</h1>
+            </header>
+
+            <main>
+                <div className="search-box">
+                    <input
+                        type="text"
+                        className="search-bar"
+                        placeholder="Enter City name"
+                        value={city}
+                        onChange={(e) => setCity(e.target.value)}
+                    />
+                    <button className="search-button">Search</button>
+                </div>
+
+                {loading && <p>Loading</p>}
+                {error && <p className="error-message">{error}</p>}
+
+                {weatherData && <div className="weather-data-container"></div>}
+            </main>
+        </div>
+    );
 }
 
 export default App;
